12d69ea21814c78b9e2fae33e86e740031b39ceb
Benedikt Grabenmeier <benedikt.grabenmeier@t-systems.com>
2019-10-08 11:29:47 +0200

    init

---
+++ src/main/java/com/ocup/SumService/SumController.java
@@ -0,0 +1,23 @@
+package com.ocup.SumService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SumController {
+	Logger logger = LoggerFactory.getLogger(SumController.class);
+	
+	static {
+		System.out.println("hier");
+	}
+
+	@RequestMapping("/sum")
+	public Double sum(@RequestParam(value = "val1", defaultValue = "0") double val1,
+			@RequestParam(value = "val2", defaultValue = "0") double val2) {
+		logger.debug("SUM: " + val1 + " + " + val2);
+		return val1 + val2;
+	}
+}
